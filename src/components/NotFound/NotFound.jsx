55e9463eb39e83197a261a44fe28ee1a77ed6a26
Charul Bari <baricharul9@hotmail.com>
2020-04-19 00:13:03 +0530

    refactor: error pages

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -4,7 +4,7 @@ import styles from "./NotFound.module.css"
 export default function NotFound(){
     return (
         <div className={styles.display}>
-            <h1 className="display-1 text-secondary">
+            <h1 className="text-secondary">
                 404 Not Found
             </h1>
         </div>
